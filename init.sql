db04cb45c2dddfb2ebcfc93e79770bed55e69171
Edoardo Fava <f.edoardo.0@gmail.com>
2023-01-14 23:45:41 +0100

    create statements 3 sections order inverted

--- init.sql
+++ init.sql
@@ -1,79 +1,4 @@
 --
--- Clean data
---
-
--- Table for Simpsons characters (non canonical version of the same character should be merged with the original one)
-CREATE TABLE character (
-    normalized_name varchar(128) PRIMARY KEY, -- Normalized known_as property
-    known_as varchar(128) NOT NULL, -- Original name not normalized
-    fandom_url varchar(256) UNIQUE, -- Foreign key to raw data
-    creation_time timestamp NOT NULL DEFAULT NOW(), -- Creation time of the data row
-    -- Constraints
-    CONSTRAINT fk_scraping_fandom FOREIGN KEY(fandom_url) REFERENCES scraping_fandom_character(fandom_url)
-);
-
--- Table for all character aliases and in general the different ways that are called
-CREATE TABLE alias (
-    normalized_alias varchar(128) PRIMARY KEY, -- Normalized alias property
-    alias varchar(128) NOT NULL, -- How the character is called
-    "character" varchar(128) NOT NULL, -- The character (foreign key)
-    creation_time timestamp NOT NULL DEFAULT NOW(), -- Creation time of the data row
-    -- Constraints
-    CONSTRAINT fk_character FOREIGN KEY("character") REFERENCES "character"(normalized_name)
-);
-
--- Table for Simpsons episodes (not future episodes)
-CREATE TABLE episode (
-    episode_number_absolute smallint PRIMARY KEY,
-    episode_number_relative smallint NOT NULL,
-    season smallint NOT NULL,
-    title varchar(128) NOT NULL,
-    rating smallint,
-    reviews_amount integer,
-    main_characters varchar(384),
-    fandom_url varchar(256) UNIQUE,
-    imdb_url varchar(256) UNIQUE,
-    creation_time timestamp NOT NULL DEFAULT NOW()
-);
-
--- Table linking every episode to their main characters (through the alias table)
-CREATE TABLE main_character (
-    episode smallint,
-    alias varchar(128),
-    -- Constraints
-    PRIMARY KEY (episode, alias),
-    CONSTRAINT fk_episode FOREIGN KEY(episode) REFERENCES episode(episode_number_absolute),
-    CONSTRAINT fk_alias FOREIGN KEY(alias) REFERENCES alias(normalized_alias)
-);
-
---
--- Raw data by entity
---
--- In these views raw data of different sources of the same entity are joined to be put side to side
---
-
-CREATE VIEW raw_character AS SELECT
-    known_as,
-    full_name,
-    image_url,
-    age,
-    species,
-    gender,
-    status,
-    fictional,
-    alias,
-    hair_color,
-    color,
-    birth_country,
-    job,
-    first_appearance,
-    first_mentioned,
-    voice,
-    fandom_url,
-    creation_time
-FROM scraping_fandom_character;
-
---
 -- Raw data by source
 --
 -- In these tables raw data is imported
@@ -130,3 +55,78 @@ CREATE TABLE scraping_imdb_episode (
     creation_time timestamp NOT NULL DEFAULT NOW()
 );
 CREATE INDEX scraping_imdb_episode_episode_number_absolute ON scraping_imdb_episode (episode_number_absolute);
+
+--
+-- Raw data by entity
+--
+-- In these views raw data of different sources of the same entity are joined to be put side to side
+--
+
+CREATE VIEW raw_character AS SELECT
+    known_as,
+    full_name,
+    image_url,
+    age,
+    species,
+    gender,
+    status,
+    fictional,
+    alias,
+    hair_color,
+    color,
+    birth_country,
+    job,
+    first_appearance,
+    first_mentioned,
+    voice,
+    fandom_url,
+    creation_time
+FROM scraping_fandom_character;
+
+--
+-- Clean data
+--
+
+-- Table for Simpsons characters (non canonical version of the same character should be merged with the original one)
+CREATE TABLE character (
+    normalized_name varchar(128) PRIMARY KEY, -- Normalized known_as property
+    known_as varchar(128) NOT NULL, -- Original name not normalized
+    fandom_url varchar(256) UNIQUE, -- Foreign key to raw data
+    creation_time timestamp NOT NULL DEFAULT NOW(), -- Creation time of the data row
+    -- Constraints
+    CONSTRAINT fk_scraping_fandom FOREIGN KEY(fandom_url) REFERENCES scraping_fandom_character(fandom_url)
+);
+
+-- Table for all character aliases and in general the different ways that are called
+CREATE TABLE alias (
+    normalized_alias varchar(128) PRIMARY KEY, -- Normalized alias property
+    alias varchar(128) NOT NULL, -- How the character is called
+    "character" varchar(128) NOT NULL, -- The character (foreign key)
+    creation_time timestamp NOT NULL DEFAULT NOW(), -- Creation time of the data row
+    -- Constraints
+    CONSTRAINT fk_character FOREIGN KEY("character") REFERENCES "character"(normalized_name)
+);
+
+-- Table for Simpsons episodes (not future episodes)
+CREATE TABLE episode (
+    episode_number_absolute smallint PRIMARY KEY,
+    episode_number_relative smallint NOT NULL,
+    season smallint NOT NULL,
+    title varchar(128) NOT NULL,
+    rating smallint,
+    reviews_amount integer,
+    main_characters varchar(384),
+    fandom_url varchar(256) UNIQUE,
+    imdb_url varchar(256) UNIQUE,
+    creation_time timestamp NOT NULL DEFAULT NOW()
+);
+
+-- Table linking every episode to their main characters (through the alias table)
+CREATE TABLE main_character (
+    episode smallint,
+    alias varchar(128),
+    -- Constraints
+    PRIMARY KEY (episode, alias),
+    CONSTRAINT fk_episode FOREIGN KEY(episode) REFERENCES episode(episode_number_absolute),
+    CONSTRAINT fk_alias FOREIGN KEY(alias) REFERENCES alias(normalized_alias)
+);
